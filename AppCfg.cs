4222c0ec7f93fafdea2f45210b4b387d76b1af38
Carlos Carrera <thewingz@gmail.com>
2017-05-09 22:08:25 +0200

    Añadir conectividad básica

--- AppCfg.cs
+++ AppCfg.cs
@@ -4,7 +4,7 @@
     {
         public int MusicVolume { get; set; }
         public int SoundVolume { get; set; }
-        public int ServerIP { get; set; }
+        public string ServerIP { get; set; }
         public int Port { get; set; }
     }
 }
---
+++ ConnectionManager.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ProyectoDAM
+{
+    class ConnectionManager
+    {
+        public void Connect(string IP, int port)
+        {
+            try
+            {
+                IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IP), port);
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                s.Connect(ep);
+                //s.SendTimeout(5);
+
+                NetworkStream ns = new NetworkStream(s);
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+
+                if (sr.ReadLine() != "ConectadoASideShooting")
+                {
+                    throw new SocketException();
+                }
+                else
+                {
+                    //gestionar una vez conectado
+                }
+            }
+            catch (SocketException e)
+            {
+            }
+        }
+    }
+}
