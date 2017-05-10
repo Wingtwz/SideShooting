6161f3483695f0dd26259d4ef7d3a946ceb722a9
Carlos Carrera <thewingz@gmail.com>
2017-05-10 10:33:24 +0200

    Añadir más assets

---
+++ Protagonist.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace ProyectoDAM
+{
+    public class Protagonist
+    {
+        public Texture2D Sprite { get; set; }
+
+        public Protagonist(Texture2D sprite)
+        {
+            this.Sprite = sprite;
+        }
+    }
+}
---
+++ Screens/GameScreen.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProyectoDAM.Screens
+{
+    public class GameScreen : Screen
+    {
+        private Texture2D spriteProt;
+        private Texture2D map;
+
+        public GameScreen(ContentManager content, GraphicsDevice graphics) : base (content, graphics)
+        {
+            this.LoadContent();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch.Begin();
+
+            SpriteBatch.Draw(map, new Vector2(0), Color.White);
+
+            SpriteBatch.End();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void LoadContent()
+        {
+            spriteProt = Content.Load<Texture2D>("Images/character");
+            map = Content.Load<Texture2D>("Images/mapahierba");
+        }
+    }
+}
