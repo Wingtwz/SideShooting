18db958971c0a3cf652396296a0b2446bc410808
Carlos Carrera <thewingz@gmail.com>
2017-05-23 00:02:38 +0200

    Añadir texto de creditos y ayuda

--- Screens/CreditsScreen.cs
+++ Screens/CreditsScreen.cs
@@ -11,13 +11,16 @@ namespace SideShooting.Screens
 
         private Texture2D bgImage;
         private SpriteFont titleFont, messageFont;
-        private string message = "Juego hecho por";
+        private string message = "SideShooting por Carlos Carrera.\n" +
+            "Interfaz de usuario por Buch. Sprites del personaje y casillas para el mapa por ArMM1998.\n" +
+            "Pistas de musica de SUPER GAME MUSIC. Fuentes de texto de Aaron D. Chand y prask.\n" +
+            "El resto de elementos, construccion del mapa, arte, sonidos y programacion hecho por Carlos Carrera.";
 
         public CreditsScreen(ContentManager content, GraphicsDevice graphicsDevice) : base(content, graphicsDevice)
         {
             this.LoadContent();
 
-            backRect = new Rectangle(500, 500, 320, 80);
+            backRect = new Rectangle(550, 550, 320, 80);
         }
 
         public override void LoadContent()
@@ -34,7 +37,7 @@ namespace SideShooting.Screens
             SpriteBatch.Draw(bgImage, new Vector2(0), Color.White);
 
             SpriteBatch.DrawString(titleFont, "Atras", new Vector2(backRect.X, backRect.Y), Color.White);
-            SpriteBatch.DrawString(messageFont, message, new Vector2(150, 320), Color.White);
+            SpriteBatch.DrawString(messageFont, message, new Vector2(70, 350), Color.White);
 
             SpriteBatch.End();
         }
--- Screens/HelpScreen.cs
+++ Screens/HelpScreen.cs
@@ -11,13 +11,23 @@ namespace SideShooting.Screens
 
         private Texture2D bgImage;
         private SpriteFont titleFont, messageFont;
-        private string message = "SideShooting es un juego en linea para dos jugadores...";
+        private string message = "SideShooting es un juego en linea para dos jugadores de vista lateral.\n" +
+            "Una vez conectados dos jugadores, la partida comenzara.\n" +
+            "El objetivo es derrotar al jugador acertandole con disparos, y al mismo tiempo esquivar los del rival.\n" +
+            "Cada jugador puede aguantar hasta 6 disparos antes de perder.\n" +
+            "Los controles son:\n" +
+            "W/A/S/D: Moverse hacia arriba/izquierda/abajo/derecha/n" +
+            "Clic izquierdo: disparar en direccion al cursor del raton.\n" +
+            "Clic derecho: aceleron - permite moverse mas rapido durante un corto espacio de tiempo.\n" +
+            "Q: fogueo - permite borrar todos los proyectiles que existen en el mapa.\n\n" +
+            "Ten en cuenta que aceleron solo estara disponible si la barra verde esta al maximo, y fogueo si lo esta la barra azul.\n" +
+            "Ademas, solo podras tener un maximo de 9 proyectiles a la vez en el mapa, el numero restante se indica en la interfaz.";
 
         public HelpScreen(ContentManager content, GraphicsDevice graphicsDevice) : base(content, graphicsDevice)
         {
             this.LoadContent();
             
-            backRect = new Rectangle(500, 500, 320, 80);
+            backRect = new Rectangle(550, 620, 320, 80);
         }
 
         public override void LoadContent()
@@ -34,7 +44,7 @@ namespace SideShooting.Screens
             SpriteBatch.Draw(bgImage, new Vector2(0), Color.White);
 
             SpriteBatch.DrawString(titleFont, "Atras", new Vector2(backRect.X, backRect.Y), Color.White);
-            SpriteBatch.DrawString(messageFont, message, new Vector2(150, 320), Color.White);
+            SpriteBatch.DrawString(messageFont, message, new Vector2(70, 300), Color.White);
 
             SpriteBatch.End();
         }
